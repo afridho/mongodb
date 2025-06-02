71bd057b112997c7e9325dc2b74701f9934c11f1
afridho <afridho@dxtr.asia>
2025-06-02 11:35:44 +0700

    update-package

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ export { ClientDB };
 // CommonJS export for compatibility
 // @ts-ignore
 if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
-    module.exports = {
-        default: ClientDB,
-        ClientDB,
-    };
+    module.exports = ClientDB;
+    module.exports.ClientDB = ClientDB;
 }
