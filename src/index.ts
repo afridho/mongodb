cbae0b8849bc106062ae9e2a0eecd5af6ff586f0
afridho <afridho@dxtr.asia>
2025-08-27 10:59:26 +0700

    fix(objectId)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import ClientDB from "./connectdb";
+import ClientDB, { ObjectId } from "./connectdb";
 
 // ES Module exports
 export default ClientDB;
-export { ClientDB };
+export { ClientDB, ObjectId };
 
 // CommonJS export for compatibility
 // @ts-ignore
 if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
     module.exports = ClientDB;
     module.exports.ClientDB = ClientDB;
+    module.exports.ObjectId = ObjectId;
 }
